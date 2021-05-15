59bed0e437c3f5865d7d765b0664a530ee4080ba
Michael Baez <mrbaez427@gmail.com>
2021-05-15 12:40:44 -0400

    Added link to StudentList component to route to SingleStudent component

--- client/components/StudentList.js
+++ client/components/StudentList.js
@@ -1,40 +1,45 @@
 import React from 'react';
-import {fetchStudents} from '../redux/store';
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+import { Route, Link } from 'react-router-dom';
+import { fetchStudents } from '../redux/store';
+import { connect } from 'react-redux';
+import SingleStudent from './SingleStudent';
 
 class StudentList extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
   }
 
-  componentDidMount() {
+  componentDidMount () {
     this.props.loadStudents();
   }
 
-  render() {
+  render () {
     return (
       <ul>
-        {this.props.students.map((student) => (
+        {this.props.students.map(student => (
           <li key={student.id}>
             <div>
               <p>Name: {student.fullName}</p>
               <p>Email: {student.email}</p>
+              <Link to='/students/:studentId'>View Details</Link>
+              <Route
+                path='/students/:studentId'
+                component={SingleStudent}
+              ></Route>
             </div>
           </li>
         ))}
       </ul>
-    )
-
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   students: state.students
 });
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   loadStudents: () => dispatch(fetchStudents())
-})
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(StudentList);
